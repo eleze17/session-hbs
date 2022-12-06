5eb13012ff33477059de76ab5b081a6fa19ee217
eleze17 <ezeenriquez17@gmail.com>
2022-12-06 18:10:42 -0300

    redireccionado ok

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 let btn =  document.getElementById('enviarUsuario')
 
-btn.addEventListener('click' , async ()=>{
+btn.addEventListener('click' ,async  ()=>{
     let usuario ={} 
     usuario.nombre= document.getElementById('floatingInput').value
     let user =  await fetch('/api/formularios',{
@@ -8,10 +8,15 @@ btn.addEventListener('click' , async ()=>{
     body:JSON.stringify(usuario),
     headers:{'Content-Type': 'application/json'},
     redirect: 'follow'
-    
-})
+    })
+    window.location.href = 'http://localhost:8080/api/formulario';
+    console.log(window.location.href)
+
 }
 )
+
+
+
 
 
 
